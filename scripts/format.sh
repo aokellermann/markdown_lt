1ee0b762b054abd92a14a5b62cfda2a7e69fb663
Antony Kellermann <aokellermann@gmail.com>
2021-01-13 15:05:24 -0500

    Use two spaces in bash for indent

--- scripts/format.sh
+++ scripts/format.sh
@@ -3,12 +3,12 @@
 set -eo pipefail
 
 if [ "$#" -eq 0 ]; then
-	yapf -d -r markdown_lt setup.py
-	shfmt -l -d scripts
+  yapf -d -r markdown_lt setup.py
+  shfmt -l -d -i 2 scripts
 elif [ "$1" == "-i" ]; then
-	yapf -i -r markdown_lt setup.py 2>/dev/null
-	shfmt -w scripts
+  yapf -i -r markdown_lt setup.py 2>/dev/null
+  shfmt -w -i 2 scripts
 else
-	echo "Usage: format.sh [-i]"
-	exit 1
+  echo "Usage: format.sh [-i]"
+  exit 1
 fi
