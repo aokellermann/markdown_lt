e3fb7db938071c1297ebb767a270ceeb2cbf4e36
Antony Kellermann <aokellermann@gmail.com>
2021-01-13 15:08:33 -0500

    Use full option name

--- scripts/format.sh
+++ scripts/format.sh
@@ -3,10 +3,10 @@
 set -eo pipefail
 
 if [ "$#" -eq 0 ]; then
-  yapf -d -r markdown_lt setup.py
+  yapf --diff --recursive markdown_lt setup.py
   shfmt -l -d -i 2 scripts
 elif [ "$1" == "-i" ]; then
-  yapf -i -r markdown_lt setup.py 2>/dev/null
+  yapf --in-place --recursive markdown_lt setup.py 2>/dev/null
   shfmt -w -i 2 scripts
 else
   echo "Usage: format.sh [-i]"
