0917eb2de74c915e3276e3491ea3e26b2ce937c6
Antony Kellermann <aokellermann@gmail.com>
2021-01-14 16:31:08 -0500

    full names

--- scripts/format.sh
+++ scripts/format.sh
@@ -3,10 +3,10 @@
 set -eo pipefail
 
 if [ "$#" -eq 0 ]; then
-	yapf -d -r markdown_lt setup.py
+	yapf --diff --recursive markdown_lt setup.py
 	shfmt -l -d scripts
 elif [ "$1" == "-i" ]; then
-	yapf -i -r markdown_lt setup.py 2>/dev/null
+	yapf --in-place --recursive markdown_lt setup.py 2>/dev/null
 	shfmt -w scripts
 else
 	echo "Usage: format.sh [-i]"
