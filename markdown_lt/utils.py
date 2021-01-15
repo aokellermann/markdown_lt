4df1ecd930812a5ac8b45b33e16201c0466b5e03
Antony Kellermann <aokellermann@gmail.com>
2021-01-14 21:26:44 -0500

    new match to string

--- markdown_lt/utils.py
+++ markdown_lt/utils.py
@@ -13,7 +13,17 @@ def readlines_utf8(filepath: str) -> list:
         return [word.decode('utf-8').strip('\n ') for word in file.readlines()]
 
 
-def matches_to_string(matches: list):
+def match_to_string(match) -> str:
+    """Returns a human readable string of a match."""
+    s = "{} [{}]".format(match.message, match.ruleId)
+    if match.replacements:
+        s += '\nSuggestion: {}'.format('; '.join(match.replacements))
+    s += '\n{}\n{}'.format(match.context, ' ' * match.offsetInContext + '^' * match.errorLength)
+    return s
+
+
+def matches_to_string(matches: list) -> str:
     """Returns a human readable string of matches."""
     return "Found {} matches{}".format(
-        len(matches), "." if not matches else ":\n{}".format("\n\n".join([str(match) for match in matches])))
+        len(matches),
+        "." if not matches else ":\n{}".format("\n\n".join([match_to_string(match) for match in matches])))
