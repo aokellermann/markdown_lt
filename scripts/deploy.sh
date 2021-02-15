ca066164ee5e6fa17600448217ded668092d4957
Antony Kellermann <aokellermann@gmail.com>
2021-02-15 14:01:53 -0500

    Update deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,15 +2,17 @@
 
 set -eo pipefail
 
-git checkout -q @^
-previous_version="$(python -c 'import markdown_lt; print(markdown_lt.__version__)')"
+if [ "$CIRCLE_BRANCH" = "master" ]; then
+  git checkout -q @^
+  previous_version="$(python -c 'import markdown_lt; print(markdown_lt.__version__)')"
 
-git checkout -q feature/Spelling
-current_version="$(python -c 'import markdown_lt; print(markdown_lt.__version__)')"
+  git checkout -q master
+  current_version="$(python -c 'import markdown_lt; print(markdown_lt.__version__)')"
 
-if [ "$previous_version" != "$current_version" ] && [ "$CIRCLE_BRANCH" = "master" ]; then
-  rm -rf build/ dist/
-  python setup.py sdist bdist_wheel
-  twine check dist/*
-  twine upload dist/* --verbose --username "__token__" --password "$PYPI_TOKEN_MARKDOWN_LT"
+  if [ "$previous_version" != "$current_version" ]; then
+    rm -rf build/ dist/
+    python setup.py sdist bdist_wheel
+    twine check dist/*
+    twine upload dist/* --verbose --username "__token__" --password "$PYPI_TOKEN_MARKDOWN_LT"
+  fi
 fi
